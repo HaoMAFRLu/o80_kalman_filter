5726191108f9b463a262758376bfad31701c2185
hma <hao.ma@tuebingen.mpg.de>
2024-01-17 14:09:53 +0100

    Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# o80_kalman_filter
+
+## Introduction
--- __main__.py
+++ __main__.py
@@ -23,14 +23,14 @@ def run():
             velocity = (trajectory[i, :]-trajectory[i-1, :])/((iterations[i]-iterations[i-1])*1e-6)
     
         ball.input_data(t, position, velocity, omega)
-        if i == 50:
+        if i == 30:
             trajectory_prediction = ball.prediction(ball.ball["states"], 1.0, 0.01)
     
     states_measurement = np.array(ball.ball["states_measurement"])
     states_kf = np.array(ball.ball["states_kf"])
-    
+
     # fcs.plot_trajectories(states_measurement[:, 0:3], states_kf[:, 0:3], table=table)
-    fcs.plot_trajectories(trajectory, trajectory_prediction, table=table)
+    fcs.plot_trajectories(states_kf[:, 0:3], trajectory_prediction, table=table)
 
 if __name__ == '__main__':
     run()
